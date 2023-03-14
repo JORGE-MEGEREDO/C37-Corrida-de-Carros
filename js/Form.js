addda82331f5329b764fdbcb3beeb872295ec9cd
JORGE-MEGEREDO <114191444+JORGE-MEGEREDO@users.noreply.github.com>
2023-03-14 19:03:24 -0300

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -1,50 +1,48 @@
 class Form {
   constructor() {
-this.input = createInput("").attribute("placeholder", "Digite seu nome"); 
-this.playButton = createButton("Play");
-this.titleImg = createImg("./assets/title.png", "título do jogo"); 
-this.greeting = createElement("h2"); }
-=    // CRIE OS ELEMENTOS DA CLASSE
+    this.input = createInput("").attribute("placeholder", "Digite seu nome");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "título do jogo");
+    this.greeting = createElement("h2");
   }
 
-  setElementPosition() {
-this.titleImg.position (120, 100);
-this.input.position (width / 2 - 110, height/2 - 80);
-this.playButton.position (width / 2 - 90, height/2 - 20);
-this.greeting.position (width / 2 - 300, height / 2 - 100);
-    // DEFINA A POSIÇÃO DOS ELEMENTOS
+  setElementsPosition() {
+    this.titleImg.position(120, 100);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
   }
 
+ /* setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  } */
 
   hide() {
-   this.greeting.hide(); 
-   this.playButton.hide();
-   this.input.hide();
- 
-    // USE A FUNÇÃO PARA DESAPARECER OS ELEMENTOS
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
   }
 
   handleMousePressed() {
-  this.playButton.mousePressed(() => {
-  this.input.hide();
-  this.playButton.hide();
-  var message = Olá ${this.input.value()} </br>espere outro jogador entrar...`;
-  this.greeting.html (message);
-  player.name = this.input.value();
-  player.index = 1;  });
-}
-    
-    // CRIE A FUNÇÃO DE PRESSIONAMENTO DE TECLA
-
-   /* O símbolo usado para escrever a mensagem abaixo (laranja) é a crase. */
+    this.playButton.mousePressed(() => {
+      this.input.hide();
+      this.playButton.hide();
+      var message = `
+      Olá ${this.input.value()}
+      </br>espere outro jogador entrar...`;
+      this.greeting.html(message);
+      player.name = this.input.value();
+      player.index = 1;
+      //player.addPlayer();
+    });
   }
 
   display() {
    this.setElementsPosition();
-   this.handleMousePressed();  }
-}
-    
-    // CHAME A FUNÇÃO DE POSIÇÃO DOS ELEMENTOS
-    // CHAME A FUNÇÃO DE PRESSIONAMENTO DE TECLA
+    //this.setElementsStyle();
+    this.handleMousePressed();
   }
 }
